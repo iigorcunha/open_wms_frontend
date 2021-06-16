91214f09fd6f2e31d891151004d8ac83b0183f25
Igor Cunha <igorcesarcunha@gmail.com>
2021-06-16 14:19:54 -0400

    styled button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,9 +1,26 @@
-import { Button as ChakraButton } from '@chakra-ui/react';
+import {
+  Button as ChakraButton,
+  ButtonProps as ChakraButtonProps,
+} from '@chakra-ui/react';
 
-interface ButtonProps {
+interface ButtonProps extends ChakraButtonProps {
   children: React.ReactNode;
 }
 
-export function Button({ children }: ButtonProps): JSX.Element {
-  return <ChakraButton>{children}</ChakraButton>;
+export function Button({ children, ...rest }: ButtonProps): JSX.Element {
+  return (
+    <ChakraButton
+      maxW="450px"
+      w="100%"
+      borderRadius="20px"
+      h="60px"
+      bg="main.green"
+      _hover={{
+        bg: 'main.lightGreen',
+      }}
+      {...rest}
+    >
+      {children}
+    </ChakraButton>
+  );
 }
